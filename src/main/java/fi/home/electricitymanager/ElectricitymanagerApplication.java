ec4264f7c0f3064e4846d6d07ab9a2b5244e8630
Tomi Mäkinen <tomim@Tomis-MBP.P-661HNU-DI-F1>
2015-12-27 20:58:36 +0200

    Initial commit

---
+++ src/main/java/fi/home/electricitymanager/ElectricitymanagerApplication.java
@@ -0,0 +1,13 @@
+package fi.home.electricitymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ElectricitymanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectricitymanagerApplication.class, args);
+    }
+}
