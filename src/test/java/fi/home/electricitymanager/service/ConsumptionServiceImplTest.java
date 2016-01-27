5f005f3c09982a6b5535006c2b717850f64ffe53
Tomi Mäkinen <tomppa.makinen@gmail.com>
2016-01-27 21:54:27 +0200

    Improved test

--- src/test/java/fi/home/electricitymanager/service/ConsumptionServiceImplTest.java
+++ src/test/java/fi/home/electricitymanager/service/ConsumptionServiceImplTest.java
@@ -3,13 +3,16 @@ package fi.home.electricitymanager.service;
 import fi.home.electricitymanager.ElectricitymanagerApplication;
 import fi.home.electricitymanager.model.ConsumptionDTO;
 import fi.home.electricitymanager.model.Tariff;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.time.YearMonth;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ElectricitymanagerApplication.class)
@@ -28,10 +31,17 @@ public class ConsumptionServiceImplTest {
                 .year(2015)
                 .build();
 
-        consumptionService.save(dto);
-        ConsumptionDTO actual = consumptionService.get(new Long(1));
-        Assert.assertEquals(actual.getElectricityAmount(), new Long(1500));
-        Assert.assertNotNull(consumptionService.get(new Long(1)));
+        Long consumptionId = consumptionService.save(dto);
+
+        ConsumptionDTO actual = consumptionService.get(consumptionId);
+
+
+        final YearMonth november2015 = YearMonth.of(2015,11);
+
+        assertEquals(new Long(1500), actual.getElectricityAmount());
+        assertEquals(Tariff.DAY, actual.getTariff());
+        assertEquals(november2015, actual.getYearAndMonth());
+
     }
 
 }
