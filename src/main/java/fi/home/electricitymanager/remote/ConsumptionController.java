6cffd85a74ecc8c1689813bf6d83aa69c1bcf5ce
Tomi Mäkinen <tomppa.makinen@gmail.com>
2016-12-05 18:59:46 +0200

    Added request params to controller

--- src/main/java/fi/home/electricitymanager/remote/ConsumptionController.java
+++ src/main/java/fi/home/electricitymanager/remote/ConsumptionController.java
@@ -2,6 +2,8 @@ package fi.home.electricitymanager.remote;
 
 import fi.home.electricitymanager.model.ConsumptionDTO;
 import fi.home.electricitymanager.model.Tariff;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,11 +14,19 @@ public class ConsumptionController {
 
 
     @RequestMapping(value = "/consumption/save", method = RequestMethod.POST, produces = "application/json")
-    public ConsumptionDTO save(@RequestParam Long electricity) {
-        return new ConsumptionDTO.Builder().electricityAmount(electricity)
-                                    .month(1)
-                                    .year(2016)
-                                    .tariff(Tariff.DAY)
-                                    .build();
+    public ResponseEntity<ConsumptionDTO> save(@RequestParam(name = "amount") Long electricity,
+                               @RequestParam(name = "month") Integer month,
+                               @RequestParam(name = "year") Integer year,
+                               @RequestParam(name = "tariff") String tariff) {
+        try {
+            ConsumptionDTO dto = new ConsumptionDTO.Builder().electricityAmount(electricity)
+                    .month(month)
+                    .year(year)
+                    .tariff(Tariff.fromString(tariff))
+                    .build();
+            return ResponseEntity.ok(dto);
+        } catch (ConsumptionDTO.Builder.ValidationException validationException) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
     }
 }
